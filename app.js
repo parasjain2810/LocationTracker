a1987350ef72b4648f1e24f39884220fa265c359
parashjain2810 <parashjain2810@gmail.com>
2024-07-06 17:05:07 +0530

    location tracker complete

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path=require('path')
+
+app.use(express.json())
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"public")))
+app.get('/',(req,res)=>{
+    res.render("index")
+})
+
+
+
+io.on('connection', (socket) => {
+    socket.on('send-location',(data)=>{
+       io.emit('recieve-location',{id:socket.id,...data})
+    })
+  console.log('a user connected',socket.id);
+  
+  socket.on("disconnect",()=>{
+    io.emit("user-disconnect",socket.id)
+    console.log('user diconnected',socket.io);
+  })
+});
+
+server.listen(3300, () => {
+  console.log('backend connected successfully');
+});
+
